a266e915eb5dd7fa9373848fad95e69536db4a94
daostats <daostats@users.noreply.github.com>
2016-05-18 03:33:03 +0000

    Data file updates

--- tokendata/0xdd6c329c66cf520ace01c61bfa666dfb99815ee3.js
+++ tokendata/0xdd6c329c66cf520ace01c61bfa666dfb99815ee3.js
@@ -6,6 +6,7 @@
    "blk": 1536288,
    "out": 0,
    "tgt": "0x0000000000000000000000000000000000000000",
+   "tid": 11,
    "txh": "0x4f077b6c29fadc12edab99eceadbec121a4a77f7faca4fa56cad053e3df2dcdc"
   }
  ]
