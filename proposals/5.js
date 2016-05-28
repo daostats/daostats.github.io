8955def9377328d843631287f972d2f10c997b9e
daostats <daostats@users.noreply.github.com>
2016-05-28 19:50:08 +0000

    Data file updates

--- proposals/5.js
+++ proposals/5.js
@@ -5,14 +5,20 @@
 "creator": "0xd68ba7734753e2ee54103116323aba2d94c78dc5",
 "deadline": 1465673494,
 "desc": "Moratorium on proposals until the DAO contract is upgraded (via newContract proposal), fixing most serious issues with voting and splitting. Funds can be moved to the new contract gradually, via multiple proposals.",
-"history": [],
+"history": [
+[
+1601904,
+165794,
+30095
+]
+],
 "newCurator": 0,
-"nope": 0,
+"nope": 30095,
 "passed": null,
 "propID": 5,
 "propdeposit": 200,
 "qDivisor": null,
 "recipient": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
 "txhash": "0x3bd5b80b3094d253ec97beb3cf7d1b9047f47e8d95f406ae2dfc8cee2d16c9b0",
-"yes": 657944164258756231714
+"yes": 165794
 }
--- tokendata/0x5e51cc70ad2848c75b71da6dc235b377b73ec3ca.js
+++ tokendata/0x5e51cc70ad2848c75b71da6dc235b377b73ec3ca.js
@@ -1,6 +1,14 @@
 {
- "amount": 10000000000000000000,
+ "amount": 0,
  "transfers": [
+  {
+   "amt": 10000000000000000000,
+   "blk": 1601910,
+   "out": 1,
+   "tgt": "0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98",
+   "tid": 10,
+   "txh": "0x45ae2b9b9cc61e982c4b1eda47c2f43af343ff9413d28b7f44879e13e505ac9f"
+  },
   {
    "amt": 10000000000000000000,
    "blk": 1601889,
