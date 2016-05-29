7cd7c93016e1ed577524593f7cc5eb97d63b65fc
daostats <daostats@users.noreply.github.com>
2016-05-29 23:10:14 +0000

    Data file updates

--- proposals/11.js
+++ proposals/11.js
@@ -5,7 +5,13 @@
 "creator": "0x71744de37e18561c508082ea27aee707190c429b",
 "deadline": 1465765529,
 "desc": "Curators, please hire somebody to fix the DAO code and allow more recipients",
-"history": [],
+"history": [
+[
+1608791,
+50201,
+29300
+]
+],
 "newCurator": 0,
 "nope": 293000000000000000000,
 "passed": null,
@@ -14,5 +20,5 @@
 "qDivisor": null,
 "recipient": "0xda4a4626d3e16e094de3225a751aab7128e96526",
 "txhash": "0xf77a3deb7851e76f0020adee23392c2250b36ebea34e4d2161c8c08443005ae6",
-"yes": 2012212992825342694
+"yes": 502012212992825342694
 }
--- tokendata/0x7bb92e1ac6a665fb7fbf9e9569297901d24b6ff5.js
+++ tokendata/0x7bb92e1ac6a665fb7fbf9e9569297901d24b6ff5.js
@@ -1,6 +1,14 @@
 {
- "amount": 100000000000000000,
+ "amount": 0,
  "transfers": [
+  {
+   "amt": 100000000000000000,
+   "blk": 1608788,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 0,
+   "txh": "0x64b3bb7a525362caf3ff0e3125e4be7f3822bb766aa27eb41f69dbfc58fc2e37"
+  },
   {
    "amt": 100000000000000000,
    "blk": 1608783,
