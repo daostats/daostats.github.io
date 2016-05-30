da741bd5b92602354b0563b51b8076eb20ddc83f
daostats <daostats@users.noreply.github.com>
2016-05-30 11:50:07 +0000

    Data file updates

--- tokendata/0x1813e4492267b8e940b517ffe925b734853c32c1.js
+++ tokendata/0x1813e4492267b8e940b517ffe925b734853c32c1.js
@@ -1,6 +1,46 @@
 {
- "amount": 400000000000000000,
+ "amount": 0,
  "transfers": [
+  {
+   "amt": 600000000000000000,
+   "blk": 1611943,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 9,
+   "txh": "0x13ba463354e6d84f510a0fa9728b31df899a61cc385a99e78e50b0d152ab7928"
+  },
+  {
+   "amt": 200000000000000000,
+   "blk": 1611933,
+   "out": 0,
+   "tgt": "0x9fa258884d2b39ad5017b8f9b6fcb5b7ac4b1649",
+   "tid": 0,
+   "txh": "0x19f4b5f080ba3917b07ce6490bca32fc098466c31fe8916fc35ecb67ffc8f5e"
+  },
+  {
+   "amt": 200000000000000000,
+   "blk": 1611931,
+   "out": 0,
+   "tgt": "0x9fa258884d2b39ad5017b8f9b6fcb5b7ac4b1649",
+   "tid": 1,
+   "txh": "0x2b40ce681ac04e5aae007cc606907521862840e347adf4fc78e5333cd16d8fe"
+  },
+  {
+   "amt": 400000000000000000,
+   "blk": 1611930,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 14,
+   "txh": "0xcafee7a212088c740906a76034f3e9989f6da673754e4e65f008f9ec858a1279"
+  },
+  {
+   "amt": 200000000000000000,
+   "blk": 1611930,
+   "out": 0,
+   "tgt": "0x9fa258884d2b39ad5017b8f9b6fcb5b7ac4b1649",
+   "tid": 1,
+   "txh": "0xffd36fa9c748948c4a9e090cda80a6ccdde004b4ed02725f6d7022c4951a45c8"
+  },
   {
    "amt": 200000000000000000,
    "blk": 1611922,
---
+++ tokendata/0xbd5c135b569dc0cfd02211a4ddad3ff1981b8e16.js
@@ -0,0 +1,21 @@
+{
+ "amount": 0,
+ "transfers": [
+  {
+   "amt": 100000000000000000000,
+   "blk": 1611943,
+   "out": 1,
+   "tgt": "0x0a869d79a7052c7f1b55a8ebabbea3420f0d1e13",
+   "tid": 7,
+   "txh": "0x5470832412fcb8061c6d02492ddca5b22f7f04f5a9f2bb1024fba2b8ef896584"
+  },
+  {
+   "amt": 100000000000000000000,
+   "blk": 1611941,
+   "out": 0,
+   "tgt": "0xc5ee17df59b02c6051923197f746c723f7803ebf",
+   "tid": 0,
+   "txh": "0x5e064b69110907c8cc3f8105887bc01ac509fc209d2973c6d38b276dfbfbdf54"
+  }
+ ]
+}
