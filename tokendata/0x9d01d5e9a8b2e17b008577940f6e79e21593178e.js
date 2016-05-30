da0724b7ffddac75ea6c250d2760cb4c1e06b43d
daostats <daostats@users.noreply.github.com>
2016-05-30 02:20:09 +0000

    Data file updates

--- tokendata/0x9d01d5e9a8b2e17b008577940f6e79e21593178e.js
+++ tokendata/0x9d01d5e9a8b2e17b008577940f6e79e21593178e.js
@@ -1,6 +1,14 @@
 {
- "amount": 1000000000000000000,
+ "amount": 0,
  "transfers": [
+  {
+   "amt": 1000000000000000000,
+   "blk": 1609561,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 0,
+   "txh": "0xedcad17a959ce45a13c7529237a282efc2c482fe2dba15ab1b47859f657e9af9"
+  },
   {
    "amt": 1000000000000000000,
    "blk": 1609542,
---
+++ tokendata/0xe584a2f84829a0c0db9bb8738b7d0623b324972c.js
@@ -0,0 +1,21 @@
+{
+ "amount": 0,
+ "transfers": [
+  {
+   "amt": 1000000000000000000,
+   "blk": 1609561,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 4,
+   "txh": "0x7700833b7cbd0568bb12d644a92f2fddd35c80e6e82fc924c5a4921b635d78aa"
+  },
+  {
+   "amt": 1000000000000000000,
+   "blk": 1609553,
+   "out": 0,
+   "tgt": "0x6ede9d50f65c4ace79678fbc5807b2bc54a8c9fe",
+   "tid": 1,
+   "txh": "0x91a564f5a5a0924dafe0ce3d43286f43879da4264407b045a1ffd162f62b67b8"
+  }
+ ]
+}
