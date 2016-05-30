ceff194e6e2c312e9cbc0a270ac2e9ba444264de
daostats <daostats@users.noreply.github.com>
2016-05-30 14:45:07 +0000

    Data file updates

---
+++ accvotes/0x7a5dd98e9ea25fa6952097a1eaeb92ae8904d696.js
@@ -0,0 +1,8 @@
+[
+ [
+  5,
+  1,
+  "0xca165f0a61cfa873ba86c27885efcedc7296f86facbff64ca77025842f23fcf",
+  4
+ ]
+]
---
+++ tokendata/0x3fc71eeedc5a106d9410499847dfa5b1a41b5769.js
@@ -0,0 +1,21 @@
+{
+ "amount": 0,
+ "transfers": [
+  {
+   "amt": 9000000000000000,
+   "blk": 1612687,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 6,
+   "txh": "0x623f2b8e8236b1f1c84e3c486b25b204b989fc47fc40d044f4e6d663cae9303b"
+  },
+  {
+   "amt": 9000000000000000,
+   "blk": 1612669,
+   "out": 0,
+   "tgt": "0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98",
+   "tid": 0,
+   "txh": "0x5a86bb7bc4dc117e80b9b547499100cb8e1bb6e10990bc33c900cfd83f40799c"
+  }
+ ]
+}
