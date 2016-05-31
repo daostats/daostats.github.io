da8d7aa528af086b148b991edf3ec3265184e31d
daostats <daostats@users.noreply.github.com>
2016-05-31 04:30:06 +0000

    Data file updates

--- proposals/15.js
+++ proposals/15.js
@@ -5,7 +5,13 @@
 "creator": "0x0cddbbf2871745b092e5b91eed3066e890999b60",
 "deadline": 1465835229,
 "desc": "Dear DAO - Tokenholders, I am a simple DAO-Tokenholder and I will not post anything on the conventional Web2.0 social media. At the moment, it seems voting in the DAO has security issues. I am asking all DAO-Tokenholders to show the actual voting-power of the DAO. Please vote YES to show you are actually voting. Please vote NO to show that you are voting but unfortunately, you are a little bit annoyed by that proposal. I am serious. That's why I am willing to spend 2 ETH for this proposal. Lets show what we've got! The DAO is alive! I am auxborg",
-"history": [],
+"history": [
+[
+1616120,
+5414081,
+984996
+]
+],
 "newCurator": 0,
 "nope": 9849964316227683312285,
 "passed": null,
@@ -14,5 +20,5 @@
 "qDivisor": null,
 "recipient": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
 "txhash": "0xac5f5af8d8ed1bbb7a6b277fa189b5761b5d637173799edbe24af29d16064e36",
-"yes": 54139774958873285602902
+"yes": 54140813420411747141363
 }
---
+++ tokendata/0x8114bf242a2afb1c9f82345a42657a4550c97840.js
@@ -0,0 +1,13 @@
+{
+ "amount": 400000000000000,
+ "transfers": [
+  {
+   "amt": 400000000000000,
+   "blk": 1616125,
+   "out": 0,
+   "tgt": "0x0a869d79a7052c7f1b55a8ebabbea3420f0d1e13",
+   "tid": 1,
+   "txh": "0xa011ae017df9c96ff51e4034d9b79102cebe3b99c23b7c0fdfef05c295c9ff27"
+  }
+ ]
+}
