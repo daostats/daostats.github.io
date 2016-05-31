a13441cff41c8cfe4c1d0a2707283744aba3b3db
daostats <daostats@users.noreply.github.com>
2016-05-31 10:30:15 +0000

    Data file updates

--- tokendata/0x133f4dd24108e87ab7a3d5fae408b00feeb57120.js
+++ tokendata/0x133f4dd24108e87ab7a3d5fae408b00feeb57120.js
@@ -1,6 +1,54 @@
 {
- "amount": 429040000000000000000,
+ "amount": 1000000000000000000000,
  "transfers": [
+  {
+   "amt": 70960000000000000000,
+   "blk": 1617606,
+   "out": 0,
+   "tgt": "0xe62bc4213a149cd979eeb2518f82fa4796d0c781",
+   "tid": 23,
+   "txh": "0xf5b6feb6edad8fb9e579dfc1a4ec5983d4dc40dc078a57648d97314bcaf06e84"
+  },
+  {
+   "amt": 100000000000000000000,
+   "blk": 1617603,
+   "out": 0,
+   "tgt": "0xca6b7fa5303ad7fa0448af47dd93d6406f95d776",
+   "tid": 1,
+   "txh": "0xe8bb60e5328411aafb21aabec3d93f48f3c32a5af4030fff330419bd6e40beab"
+  },
+  {
+   "amt": 100000000000000000000,
+   "blk": 1617601,
+   "out": 0,
+   "tgt": "0xb38e359682baf16fdf4b9b800387cf974c373754",
+   "tid": 1,
+   "txh": "0x1bcefa98b935697dd97a8eaf0ba8e8df4d0d0143f9cec1869c070e39be77c630"
+  },
+  {
+   "amt": 100000000000000000000,
+   "blk": 1617598,
+   "out": 0,
+   "tgt": "0x44f425a2c9b4438347972e9a62a5798a0336ae90",
+   "tid": 4,
+   "txh": "0xd0fba17607cd4bdf4c73b5a9fe591b541237e98c8b71a55cedfe0f1438665da6"
+  },
+  {
+   "amt": 100000000000000000000,
+   "blk": 1617596,
+   "out": 0,
+   "tgt": "0xaf5116dc8b802224cbb31d3d730254f5b811d1d9",
+   "tid": 1,
+   "txh": "0x1c217bdd33ffb7f400e60b6b960b52fac85e3a3e4aba620131296aa93cedd06f"
+  },
+  {
+   "amt": 100000000000000000000,
+   "blk": 1617593,
+   "out": 0,
+   "tgt": "0x826896cf1994fbb58f379055985ba3432b8e5ee6",
+   "tid": 0,
+   "txh": "0x66e302b1586fc4ef03e27c44cf8234a2eda270e444373b8d9b146d140a836cdf"
+  },
   {
    "amt": 429040000000000000000,
    "blk": 1550853,
--- tokendata/0x859cb125cd713a8bea33254fe4c57cfa1cb151ad.js
+++ tokendata/0x859cb125cd713a8bea33254fe4c57cfa1cb151ad.js
@@ -1,6 +1,22 @@
 {
- "amount": 10,
+ "amount": 0,
  "transfers": [
+  {
+   "amt": 100000000000000010,
+   "blk": 1617603,
+   "out": 1,
+   "tgt": "0x0a869d79a7052c7f1b55a8ebabbea3420f0d1e13",
+   "tid": 6,
+   "txh": "0x60c7c0db777c16010c3ac3732856f8b52d3d6f14491ccd75bf1d1425193526d8"
+  },
+  {
+   "amt": 100000000000000000,
+   "blk": 1617601,
+   "out": 0,
+   "tgt": "0x7ebac0b4507ee4de4cec3d8230d265e090172b63",
+   "tid": 0,
+   "txh": "0x338affcdc212e3827ff3c53718aecf1905d4d9387ef3fffd1e49b67332eb4106"
+  },
   {
    "amt": 10,
    "blk": 1608142,
