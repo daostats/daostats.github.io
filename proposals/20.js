297629d89eb57f31b6f3fd2bb4fab1d4b1c523c6
daostats <daostats@users.noreply.github.com>
2016-05-31 09:25:05 +0000

    Data file updates

--- proposals/20.js
+++ proposals/20.js
@@ -5,7 +5,13 @@
 "creator": "0xe7535ddfcbefe5c318d271476d068d5f7cf77290",
 "deadline": 1465290511,
 "desc": "",
-"history": [],
+"history": [
+[
+1617345,
+1000,
+0
+]
+],
 "newCurator": 1,
 "nope": 0,
 "passed": null,
@@ -14,5 +20,5 @@
 "qDivisor": null,
 "recipient": "0xe7535ddfcbefe5c318d271476d068d5f7cf77290",
 "txhash": "0x1691c991d366d621289dd63d00068844f5f8afc4bf54238faaf63c3b325741a8",
-"yes": 0
+"yes": 10000000000000000000
 }
