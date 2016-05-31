baf933a321c6b0443cb69879a05a6029c06bf972
daostats <daostats@users.noreply.github.com>
2016-05-31 02:10:07 +0000

    Data file updates

--- proposals/17.js
+++ proposals/17.js
@@ -5,7 +5,13 @@
 "creator": "0x6fc4d393bcebc69dbc8a6877e098babd19504e3b",
 "deadline": 1465868358,
 "desc": "Raising the Proposal Deposit to 11 ETH  \\n  This Proposal will raise the deposit required to make a Proposal to The DAO from 2 ETH to 11 ETH. \\n \\n An increase of the Proposal Deposit to **11 ETH** is expected to:\\n \\n \\n \\n - decrease the number of poorly thought-out proposals. \\n \\n - help avoid the confusion and concern that could be caused by hundreds of proposals that cannot be adequately assessed by DAO Token Holders. \\n \\n - reduce the number of spam proposals and amount of proposal-graffiti on The DAO. \\n \\n \\n   Moreover, the time spent on reviewing proposals by DAO Token Holders should be valued.  Therefore, additional value will accrue to The DAO because of this Proposal from the proposal deposits that stay with The DAO when proposals fail to reach quorum. \\n \\n While an increase is necessary, **11 ETH is still within reach for small start-ups and individuals** that will want to offer services to The DAO.  \\n \\n [Join the conversation about this proposal on DAOhub.org](https://forum.daohub.org/t/raising-the-proposal-deposit-to-11-eth/4106)   \\n \\n ![Graphic: decrease proposals, increase quality](https://ipfs.pics/ipfs/Qmcg2geJ2eCSMEBBSbP8Z56AZgg6mnfTHo56t4SBC74873)   \\n \\n",
-"history": [],
+"history": [
+[
+1615513,
+828722,
+1867894
+]
+],
 "newCurator": 0,
 "nope": 0,
 "passed": null,
@@ -14,5 +20,5 @@
 "qDivisor": null,
 "recipient": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
 "txhash": "0x3a7d091970d7931dd5ef4872e59093234e132f861139403d2b69db80ac2d0ecb",
-"yes": 3500000000000000000
+"yes": 163693915343915343911
 }
---
+++ tokendata/0x95d695c9060cdadcef8178ff10acf5d87b9abead.js
@@ -0,0 +1,21 @@
+{
+ "amount": 0,
+ "transfers": [
+  {
+   "amt": 500000000000000000,
+   "blk": 1615519,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 0,
+   "txh": "0x8786f3a631d1c8dbd372c229798fe002fafc8f2459f6044f58b154fff7a31a1b"
+  },
+  {
+   "amt": 500000000000000000,
+   "blk": 1615514,
+   "out": 0,
+   "tgt": "0xc0f242697811b2de549633b79de0c0d9c00e4741",
+   "tid": 10,
+   "txh": "0xdbecf05d5467c5c8380550fff3f5f1a20d7fa1125759057875fab3b9d9784d99"
+  }
+ ]
+}
