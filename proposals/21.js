4dba88c61c553658ecd5f9922bfa334859000541
daostats <daostats@users.noreply.github.com>
2016-05-31 09:15:06 +0000

    Data file updates

---
+++ proposals/21.js
@@ -0,0 +1,18 @@
+{
+"amount": 0,
+"block": 1617322,
+"closed": null,
+"creator": "0xe7535ddfcbefe5c318d271476d068d5f7cf77290",
+"deadline": 1465290854,
+"desc": "",
+"history": [],
+"newCurator": 1,
+"nope": 0,
+"passed": null,
+"propID": 21,
+"propdeposit": 0.0,
+"qDivisor": null,
+"recipient": "0xe7535ddfcbefe5c318d271476d068d5f7cf77290",
+"txhash": "0x21904450f481b1a0f182a05286fac4765216470d6e616115873c05343ad2bd26",
+"yes": 0
+}
