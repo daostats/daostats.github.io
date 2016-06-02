5ff84e985889bf10fc1430f7e79120d23f40c6e5
daostats <daostats@users.noreply.github.com>
2016-06-02 07:55:06 +0000

    Data file updates

--- tokendata/0x47e9dcf88be3d6555c491279b6b63ae31d53b87f.js
+++ tokendata/0x47e9dcf88be3d6555c491279b6b63ae31d53b87f.js
@@ -1,37 +1,37 @@
 {
- "amount": 1688518180000000000,
+ "amount": 0,
  "transfers": [
+  {
+   "amt": 300000000000000000,
+   "blk": 1628973,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 1,
+   "txh": "0x8278ded659fea0bb54d2c17984a54ec584e7964f9024702c8f9acc32ee3e567e"
+  },
   {
    "amt": 100000000000000000,
    "blk": 1628960,
-   "out": 1,
-   "tgt": "0x47e9dcf88be3d6555c491279b6b63ae31d53b87f",
+   "out": 0,
+   "tgt": "0xcd3cd6cafe8f405433e56d558efca4337fdf82cd",
    "tid": 6,
    "txh": "0x35c3c2a0065de1566d57e46921814d86e2e6f742cf5822d4ce110ff8f6541d2a"
   },
   {
    "amt": 100000000000000000,
    "blk": 1628960,
-   "out": 1,
-   "tgt": "0x47e9dcf88be3d6555c491279b6b63ae31d53b87f",
+   "out": 0,
+   "tgt": "0xcd3cd6cafe8f405433e56d558efca4337fdf82cd",
    "tid": 8,
    "txh": "0x9655cd857cf5d61dcbf48a387543d639b5584ebbe23f2e0332cbc4c6b3a2d6b4"
   },
   {
    "amt": 100000000000000000,
    "blk": 1628960,
-   "out": 1,
-   "tgt": "0x47e9dcf88be3d6555c491279b6b63ae31d53b87f",
+   "out": 0,
+   "tgt": "0xcd3cd6cafe8f405433e56d558efca4337fdf82cd",
    "tid": 9,
    "txh": "0x9b70c2e2c45c2c810cffc53f7494acba2befc2c8dd71324d46669eede2e175ab"
-  },
-  {
-   "amt": 1988518180000000000,
-   "blk": 1504340,
-   "out": 0,
-   "tgt": "0x0000000000000000000000000000000000000000",
-   "tid": 0,
-   "txh": "0x8e44ad2e4be25c4d51ca6ac3c596322b3076fbe1ead7890740f2615f440a40dd"
   }
  ]
 }
---
+++ tokendata/0xcff34b875dca759e433d440faa25c31be186c4db.js
@@ -0,0 +1,13 @@
+{
+ "amount": 41245278531300000000,
+ "transfers": [
+  {
+   "amt": 41245278531300000000,
+   "blk": 1628976,
+   "out": 0,
+   "tgt": "0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98",
+   "tid": 12,
+   "txh": "0x5cfb0f1f2f54ab5f9d860fdaac20fee350b2b0102fded1e9aabe0a68ee127b7a"
+  }
+ ]
+}
