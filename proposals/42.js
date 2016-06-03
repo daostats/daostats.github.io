7263672fcc898bbe12f3b1eadf23aca36f99a3a0
daostats <daostats@users.noreply.github.com>
2016-06-03 06:25:36 +0000

    Data file updates

---
+++ proposals/42.js
@@ -0,0 +1,18 @@
+{
+"amount": 0,
+"block": 1634681,
+"closed": null,
+"creator": "0x18e53243981aabc8767da10c73449f1391560eaa",
+"deadline": 1465539797,
+"desc": "",
+"history": [],
+"newCurator": 1,
+"nope": 0,
+"passed": null,
+"propID": 42,
+"propdeposit": 0.0,
+"qDivisor": null,
+"recipient": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
+"txhash": "0xb6f0a090c5f2705fa2731c04519aa93056ad034ea9dd54a6a921786fef26ac58",
+"yes": 0
+}
