4364a990ebe3852b6e7317004a067af12f8441b7
daostats <daostats@users.noreply.github.com>
2016-06-03 06:35:29 +0000

    Data file updates

--- proposals/42.js
+++ proposals/42.js
@@ -5,7 +5,13 @@
 "creator": "0x18e53243981aabc8767da10c73449f1391560eaa",
 "deadline": 1465539797,
 "desc": "",
-"history": [],
+"history": [
+[
+1634716,
+54977,
+0
+]
+],
 "newCurator": 1,
 "nope": 0,
 "passed": null,
@@ -14,5 +20,5 @@
 "qDivisor": null,
 "recipient": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
 "txhash": "0xb6f0a090c5f2705fa2731c04519aa93056ad034ea9dd54a6a921786fef26ac58",
-"yes": 0
+"yes": 549772840000000000000
 }
