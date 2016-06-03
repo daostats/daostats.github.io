8e6053f7b98317f936bffb141b1a4aadb42dcc19
daostats <daostats@users.noreply.github.com>
2016-06-03 06:05:06 +0000

    Data file updates

---
+++ proposals/41.js
@@ -0,0 +1,18 @@
+{
+"amount": 0,
+"block": 1634610,
+"closed": null,
+"creator": "0xfaed3f06255794bf3f83d7ab08d4554d5d218b41",
+"deadline": 1465538643,
+"desc": "",
+"history": [],
+"newCurator": 1,
+"nope": 0,
+"passed": null,
+"propID": 41,
+"propdeposit": 0.0,
+"qDivisor": null,
+"recipient": "0xfaed3f06255794bf3f83d7ab08d4554d5d218b41",
+"txhash": "0x9dbf7efea7544edc9b59aeb300efd8e830721862c42d86e2a93e471084741c3f",
+"yes": 0
+}
---
+++ tokendata/0xf1e569d540076062d564aeebc24341f60ecd7c32.js
@@ -0,0 +1,21 @@
+{
+ "amount": 0,
+ "transfers": [
+  {
+   "amt": 100000000000000000,
+   "blk": 1634593,
+   "out": 1,
+   "tgt": "0x0a869d79a7052c7f1b55a8ebabbea3420f0d1e13",
+   "tid": 5,
+   "txh": "0x96a438bce813fddbc9688b521290b99edf5e00272f2ecb4463ed533c9017bfb9"
+  },
+  {
+   "amt": 100000000000000000,
+   "blk": 1634588,
+   "out": 0,
+   "tgt": "0x080546508a3d2682c8b9884f13637b8847b44db3",
+   "tid": 2,
+   "txh": "0xac7ce49c663aaa40068a2d10cff509cfe64f2592150de6d4b7e8960a05267178"
+  }
+ ]
+}
