1d513aceac8de3d2e7cb869c5e2fc91d306cbea5
daostats <daostats@users.noreply.github.com>
2016-06-04 21:55:05 +0000

    Data file updates

---
+++ proposals/50.js
@@ -0,0 +1,18 @@
+{
+"amount": 0,
+"block": 1644553,
+"closed": null,
+"creator": "0x0ee82c5e35cbcf9e1271808f0386b930ee8ae8a2",
+"deadline": 1465681808,
+"desc": "m split",
+"history": [],
+"newCurator": 1,
+"nope": 0,
+"passed": null,
+"propID": 50,
+"propdeposit": 0.0,
+"qDivisor": null,
+"recipient": "0x0ee82c5e35cbcf9e1271808f0386b930ee8ae8a2",
+"txhash": "0x3dba8810ada46eca0b80e87ef0f6ccfa9cc0fc2bb2f45b642e65aa94df041ed8",
+"yes": 0
+}
