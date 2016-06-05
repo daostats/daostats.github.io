8229235dc14c2509ebcd57fc1232a00be1332895
daostats <daostats@users.noreply.github.com>
2016-06-05 18:05:06 +0000

    Data file updates

---
+++ proposals/51.js
@@ -0,0 +1,18 @@
+{
+"amount": 0,
+"block": 1649587,
+"closed": null,
+"creator": "0x0cddbbf2871745b092e5b91eed3066e890999b60",
+"deadline": 1466359188,
+"desc": "#42 ULTIMATE SHOW OFF DAO Community! Let\u00b4s show DAO\u00b4s voting power! Simply vote yay or nay - it doesen\u00b4t matter.  BUT VOTE! (I beg you!)",
+"history": [],
+"newCurator": 0,
+"nope": 0,
+"passed": null,
+"propID": 51,
+"propdeposit": 2.0,
+"qDivisor": null,
+"recipient": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
+"txhash": "0xa65aad79b3df6f2341ec6d79a0ae56bbf1bf87ef964f6fe5982dd1c10ee41a8",
+"yes": 0
+}
