70f9ebd37504bcc278a1f896ecb0d31d27f1b62e
daostats <daostats@users.noreply.github.com>
2016-06-06 01:05:06 +0000

    Data file updates

--- proposals/51.js
+++ proposals/51.js
@@ -35,5 +35,5 @@
 "qDivisor": null,
 "recipient": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
 "txhash": "0xa65aad79b3df6f2341ec6d79a0ae56bbf1bf87ef964f6fe5982dd1c10ee41a8",
-"yes": 115604066763113333333
+"yes": 159786739113409581527
 }
