a05110468f251f424610bbe54fccc02150bb3917
daostats <daostats@users.noreply.github.com>
2016-06-06 18:35:13 +0000

    Data file updates

---
+++ proposals/53.js
@@ -0,0 +1,18 @@
+{
+"amount": 0,
+"block": 1655776,
+"closed": null,
+"creator": "0x4853143d0f5524df67a0a5bdd2fb63c76c7693f6",
+"deadline": 1465842522,
+"desc": "arbitrage ftw 2",
+"history": [],
+"newCurator": 1,
+"nope": 0,
+"passed": null,
+"propID": 53,
+"propdeposit": 0.0,
+"qDivisor": null,
+"recipient": "0x4853143d0f5524df67a0a5bdd2fb63c76c7693f6",
+"txhash": "0x83a1fcc0b36af9efe7b956428207575a3b3f600ae9a74484b83b21e4bad09243",
+"yes": 0
+}
