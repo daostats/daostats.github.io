a390ae6748e3b04af5438ebaa1f261ec3efc71dc
daostats <daostats@users.noreply.github.com>
2016-06-07 15:45:15 +0000

    Data file updates

---
+++ proposals/56.js
@@ -0,0 +1,18 @@
+{
+"amount": 0,
+"block": 1661085,
+"closed": null,
+"creator": "0x5accb9f69bb0c04465d6701bdce4d8bd0198d0f6",
+"deadline": 1465918922,
+"desc": "Baby Split",
+"history": [],
+"newCurator": 1,
+"nope": 0,
+"passed": null,
+"propID": 56,
+"propdeposit": 0.0,
+"qDivisor": null,
+"recipient": "0x55d1554b08a5311c5c0a0b6464162192f9d2f884",
+"txhash": "0x3f62c9b56ef853b8240c427cc7d2fb9c1454cd1ddb523f3c3a1c4e70c3b725fe",
+"yes": 0
+}
