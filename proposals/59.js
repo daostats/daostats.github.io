e87232c52a73c484db1de0ad46f52b244e9ce2cc
daostats <daostats@users.noreply.github.com>
2016-06-08 11:10:08 +0000

    Data file updates

--- proposals/59.js
+++ proposals/59.js
@@ -5,9 +5,15 @@
 "creator": "0xb656b2a9c3b2416437a811e07466ca712f5a5b5a",
 "deadline": 1465969081,
 "desc": "lonely, so lonely",
-"history": [],
+"history": [
+[
+1665925,
+0,
+3838379
+]
+],
 "newCurator": 1,
-"nope": 0,
+"nope": 38383800000000000000000,
 "passed": null,
 "propID": 59,
 "propdeposit": 0.0,
