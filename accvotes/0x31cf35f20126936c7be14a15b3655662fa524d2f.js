9b5670216f1f3088a551a9a9dafabc4ad6be6c14
daostats <daostats@users.noreply.github.com>
2016-06-08 01:15:07 +0000

    Data file updates

--- accvotes/0x31cf35f20126936c7be14a15b3655662fa524d2f.js
+++ accvotes/0x31cf35f20126936c7be14a15b3655662fa524d2f.js
@@ -1,5 +1,35 @@
 [
  [
+  40,
+  1,
+  "0xe840660be1277a2bfd7532fa9fcca23a9642d21a1ccb098a8c5130fb371045d8",
+  0
+ ],
+ [
+  17,
+  1,
+  "0xecc884fbf575cb7e036e0bf315b0b84d761d37e0e9d6c4c5731337af60e02411",
+  1
+ ],
+ [
+  15,
+  1,
+  "0x84a8387adc13f8bcb42e53da113a3b4702da0d154446f470344f1a1aeeb0713",
+  0
+ ],
+ [
+  11,
+  0,
+  "0xc9b20f5b0bfdb141c61871800eaf43e59975e689bd49166bf48b4b1b6b459f5",
+  0
+ ],
+ [
+  5,
+  1,
+  "0x9f21ca73cd24de15dfd5f7e15c7ad073ed4edeaf70fecf97a64f3eaa5a594525",
+  1
+ ],
+ [
   2,
   0,
   "0xb4d2c5381c07f9bdcd6671c8613753c4af55cde1c2851a8c4e89b900fee353e1",
---
+++ tokendata/0xc3a0fc9f046761607da9ae3e230097c41bdbcc6f.js
@@ -0,0 +1,13 @@
+{
+ "amount": 100000000000000000,
+ "transfers": [
+  {
+   "amt": 100000000000000000,
+   "blk": 1663461,
+   "out": 0,
+   "tgt": "0x7da7ec4d7342b0a088f3c0e9dea9a6bf92fb5088",
+   "tid": 0,
+   "txh": "0xf902d998ba2fa147d6670c18186a0d6d3fc5cd93e822cb8464e7dd1c25da8872"
+  }
+ ]
+}
