ea9e350427d82e332fd0ee714936362a59527905
daostats <daostats@users.noreply.github.com>
2016-06-08 15:50:14 +0000

    Data file updates

---
+++ tokendata/0x4286af61364e0073f2b0ba45fe1e472f1ac11624.js
@@ -0,0 +1,21 @@
+{
+ "amount": 0,
+ "transfers": [
+  {
+   "amt": 3869000000000000000,
+   "blk": 1667118,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 1,
+   "txh": "0x7415242a5aa80572ace93fb4630897f148f26b0b9522bb746b4e02629826b19f"
+  },
+  {
+   "amt": 3869000000000000000,
+   "blk": 1667114,
+   "out": 0,
+   "tgt": "0x6ec5c914c85dfc3d2e9d5e8140fced7ec724a280",
+   "tid": 1,
+   "txh": "0x7f1ff7a7e89837c37104c508fb208d2f3e7b8bc940678090fc567852a73b5a4c"
+  }
+ ]
+}
