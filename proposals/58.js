9aa6dfed146cedad8b3ffaccb59f94cb7eb5c9da
daostats <daostats@users.noreply.github.com>
2016-06-08 10:05:05 +0000

    Data file updates

--- proposals/58.js
+++ proposals/58.js
@@ -5,7 +5,13 @@
 "creator": "0xc4dc238f4ff143480f64456e0c4837e1c802934e",
 "deadline": 1465931352,
 "desc": "this is rickdiculous!",
-"history": [],
+"history": [
+[
+1665671,
+560,
+0
+]
+],
 "newCurator": 1,
 "nope": 0,
 "passed": null,
@@ -14,5 +20,5 @@
 "qDivisor": null,
 "recipient": "0xc4dc238f4ff143480f64456e0c4837e1c802934e",
 "txhash": "0x61a54d8c4a221a8935a64e72ec43e0039391bf00d67477b3beb535f76dace472",
-"yes": 0
+"yes": 5600000000000000000
 }
---
+++ tokendata/0xd683fb6f8e684a0317b34733c205fa48302b9cd6.js
@@ -0,0 +1,21 @@
+{
+ "amount": 0,
+ "transfers": [
+  {
+   "amt": 1000000000000000000,
+   "blk": 1665662,
+   "out": 1,
+   "tgt": "0xdf21fa922215b1a56f5a6d6294e6e36c85a0acfb",
+   "tid": 1,
+   "txh": "0xc5586c34496563cc35bf6585718c9ca6c33183c53db32c87102c19e8f97a8675"
+  },
+  {
+   "amt": 1000000000000000000,
+   "blk": 1665660,
+   "out": 0,
+   "tgt": "0x897efbabb39beeddc3236cb50672411be4de75ca",
+   "tid": 1,
+   "txh": "0xaed171e32949255d91458d02b04e82bb1caa2e26a72cab81f1587efb19e1f9f4"
+  }
+ ]
+}
