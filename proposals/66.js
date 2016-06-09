7789b7beabce4587baef866aa2dc5f6cb409696f
daostats <daostats@users.noreply.github.com>
2016-06-09 13:25:06 +0000

    Data file updates

--- proposals/66.js
+++ proposals/66.js
@@ -5,7 +5,13 @@
 "creator": "0x9a9d6a470fc8034085ee8e509623e2f742da6625",
 "deadline": 1466082991,
 "desc": "THE DAO SHOWS THAT VAROUFAKIS HAS NO VISION",
-"history": [],
+"history": [
+[
+1672690,
+100000,
+0
+]
+],
 "newCurator": 1,
 "nope": 0,
 "passed": null,
@@ -14,5 +20,5 @@
 "qDivisor": null,
 "recipient": "0x9a9d6a470fc8034085ee8e509623e2f742da6625",
 "txhash": "0x57cc6b997eacf72082c9c9194d08828de8cf39c9ca7c03f76868484db37e2472",
-"yes": 0
+"yes": 1000000000000000000000
 }
