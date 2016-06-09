59d58a1649b3129e82d69e17c4fd503668f6772e
daostats <daostats@users.noreply.github.com>
2016-06-09 19:35:06 +0000

    Data file updates

--- accvotes/0xaf3bc457c3469c4376af42aee2d3f11797fc8840.js
+++ accvotes/0xaf3bc457c3469c4376af42aee2d3f11797fc8840.js
@@ -1,5 +1,11 @@
 [
  [
+  40,
+  1,
+  "0xc6819215a142a7082d07fa7b75e13b6da1bd3225ce7a951cdaf3df83e4e699c9",
+  0
+ ],
+ [
   11,
   0,
   "0xb797bb1ff762b1a892b648ce46004f8bee0cc18c4c243b2c4dcbc80ebb4cfb30",
