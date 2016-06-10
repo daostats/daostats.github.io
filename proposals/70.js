c4f0a749523fc87c70ff2c586ba6ecc64e968b5b
daostats <daostats@users.noreply.github.com>
2016-06-10 13:50:05 +0000

    Data file updates

--- proposals/70.js
+++ proposals/70.js
@@ -5,7 +5,13 @@
 "creator": "0x96dade6c87e483acb081e9f669b4fc029a440e8b",
 "deadline": 1466159754,
 "desc": "Congo Split",
-"history": [],
+"history": [
+[
+1678737,
+99700,
+0
+]
+],
 "newCurator": 1,
 "nope": 0,
 "passed": null,
@@ -14,5 +20,5 @@
 "qDivisor": null,
 "recipient": "0x55d1554b08a5311c5c0a0b6464162192f9d2f884",
 "txhash": "0x5effb1b446acf5f3e59e47d35a3de6754ef9b1c104dbb579f44e6f24734bf12c",
-"yes": 0
+"yes": 997000000000000000000
 }
