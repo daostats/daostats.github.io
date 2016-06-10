169901656a734b32644114b869227ee1d9db7bf9
daostats <daostats@users.noreply.github.com>
2016-06-10 16:10:06 +0000

    Data file updates

---
+++ proposals/71.js
@@ -0,0 +1,18 @@
+{
+"amount": 0,
+"block": 1679330,
+"closed": null,
+"creator": "0xc1aba55035310a703ee450142eb31961a2723006",
+"deadline": 1466179605,
+"desc": "beer split",
+"history": [],
+"newCurator": 1,
+"nope": 0,
+"passed": null,
+"propID": 71,
+"propdeposit": 0.0,
+"qDivisor": null,
+"recipient": "0xc1aba55035310a703ee450142eb31961a2723006",
+"txhash": "0xcf1c970114a4a7d5a99fec8057275337148af2da543c9843eb9b88da57c8301e",
+"yes": 0
+}
